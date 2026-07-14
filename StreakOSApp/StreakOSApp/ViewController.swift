5cd77790cdfaffba9bb4f3ddd5eb4bb26c25625f
Amin Faruq <aminfaruk.fa@gmail.com>
2026-07-14 12:38:06 +0800

    feat(UI): implement new iOS project

---
+++ StreakOSApp/StreakOSApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StreakOSApp
+//
+//  Created by Amin faruq on 14/07/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
