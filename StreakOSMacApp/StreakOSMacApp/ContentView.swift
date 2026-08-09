90783ebbcb9a56e1d9e7e116dd8843d775fc607c
Amin Faruq <aminfaruk.fa@gmail.com>
2026-08-10 00:25:25 +0800

    add new mac app target

---
+++ StreakOSMacApp/StreakOSMacApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StreakOSMacApp
+//
+//  Created by Amin faruq on 10/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StreakOSMacApp/StreakOSMacApp/StreakOSMacAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreakOSMacAppApp.swift
+//  StreakOSMacApp
+//
+//  Created by Amin faruq on 10/08/26.
+//
+
+import SwiftUI
+
+@main
+struct StreakOSMacAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
