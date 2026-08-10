6e522332648f983614b7a32c26e9965086dcae73
Amin Faruq <aminfaruk.fa@gmail.com>
2026-08-10 09:19:58 +0800

    feat(domain): add ItemCreator protocol for creating items

---
+++ StreakOSFramework/StreakOSFramework/Domain/Protocols/ItemCreator.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+public protocol ItemCreator {
+    typealias Result = Swift.Result<Item, Error>
+
+    func create(
+        name: String,
+        icon: String,
+        targetCount: Int,
+        startDate: Date,
+        endDate: Date?,
+        completion: @escaping (Result) -> Void
+    )
+}
