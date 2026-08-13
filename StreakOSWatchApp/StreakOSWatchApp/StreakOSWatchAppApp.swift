16d55be2a69d84a60cfc5153701141574020f511
Amin Faruq <aminfaruk.fa@gmail.com>
2026-08-13 20:16:14 +0800

    create new watchos target

---
+++ StreakOSWatchApp/StreakOSWatchApp/StreakOSWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreakOSWatchAppApp.swift
+//  StreakOSWatchApp Watch App
+//
+//  Created by Amin faruq on 13/08/26.
+//
+
+import SwiftUI
+
+@main
+struct StreakOSWatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
